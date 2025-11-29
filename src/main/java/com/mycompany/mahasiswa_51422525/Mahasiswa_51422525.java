e8b01bec790d0ac4469c0974ac06c0a5a0ad86c3
setiadyy <setiadyfriandika24@gmail.com>
2025-11-29 10:45:34 +0700

    First Commit

---
+++ src/main/java/com/mycompany/mahasiswa_51422525/Mahasiswa_51422525.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mahasiswa_51422525;
+
+/**
+ *
+ * @author UserPC
+ */
+public class Mahasiswa_51422525 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/mahasiswa_51422525/model/ModelMahasiswa.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mahasiswa_51422525.model;
+
+/**
+ *
+ * @author UserPC
+ */
+public class ModelMahasiswa {
+    private int id;
+    private String nmp;
+    private String nama;
+    private int semester;
+    private float ipk;
+    private final String npm;
+    
+    public ModelMahasiswa(int id, String npm, String nama, int semester, float ipk){
+        this.id = id;
+        this.npm = npm;
+        this.nama = nama;
+        this.semester = semester;
+        this.ipk = ipk;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNmp() {
+        return nmp;
+    }
+
+    public void setNmp(String nmp) {
+        this.nmp = nmp;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public float getIpk() {
+        return ipk;
+    }
+
+    public void setIpk(float ipk) {
+        this.ipk = ipk;
+    }
+
+    public String getNpm() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}
